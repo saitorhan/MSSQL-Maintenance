05349e757df4502d453c96fd854a212b6f06abef
Sait ORHAN <saitorhan@outlook.com>
2020-12-02 13:59:04 +0300

    columns row_number

--- Indexes.sql
+++ Indexes.sql
@@ -1,4 +1,5 @@
 select 
+ROW_NUMBER() over(partition by i.name order by i.name),
 t.name TableName, 
 i.name IndexName, 
 c.name ColumnName
@@ -6,6 +7,6 @@ from sys.tables t
 join sys.indexes i on t.object_id = i.object_id
 join sys.index_columns ic on i.index_id = ic.index_id and ic.object_id = t.object_id
 join sys.columns c on c.object_id = t.object_id and ic.column_id = c.column_id
-where t.type = 'U' and t.name like '%'
+where t.type = 'U' and t.name like 'Files'
 
 
